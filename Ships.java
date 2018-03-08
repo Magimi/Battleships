a820a6260b3e726b5173b711b42aab57dcb4b466
Magimi <36344269+Magimi@users.noreply.github.com>
2018-03-08 21:06:13 +0100

    Update Ships.java

--- Ships.java
+++ Ships.java
@@ -4,11 +4,11 @@ public class Ships {
     public static int quantityShip1 = 5;
     public static int quantityShip2 = 4;
     public static int quantityShip3 = 3;
-    public static int quantityShip4 = 1;
+    public static int quantityShip4 = 2;
     /*Ilość potrzebna do wyznaczenia wygranej*/
     public static int lenghtShip1 = 5;
     public static int lenghtShip2 = 4;
     public static int lenghtShip3 = 3;
-    public static int lenghtShip4 = 1;
+    public static int lenghtShip4 = 2;
 
 }
